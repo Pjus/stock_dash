45b15191e720e5c54919c4dffabfe515072944c9
Pjus <wnstjd117@naver.com>
2022-12-15 11:39:38 +0000

    heatmap

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import base_views
+from .views import base_views, heatmap_view
 
 app_name = 'analysis'
 
 urlpatterns = [
     path('', base_views.index, name='index'),
     path('company/', base_views.company, name='company'),
+    path('heatmap/', heatmap_view.get_heatmap, name='heatmap'),
+
 ]
--- analysis/views/heatmap_view.py
+++ analysis/views/heatmap_view.py
@@ -1,2 +1,39 @@
 from django.shortcuts import render
+from yahoo_fin import stock_info as si
+def get_heatmap(request):
 
+    snp500 = si.tickers_sp500()
+
+    tickers = []
+    deltas = []
+    sectors =[]
+    market_caps = []
+
+    for ticker in snp500:
+        
+        try:
+            ## create Ticker object
+            stock = yf.Ticker(ticker)
+            tickers.append(ticker)
+
+            ## download info
+            info = stock.info
+
+            ## download sector
+            sectors.append(info['sector'])
+
+            ## download daily stock prices for 2 days
+            hist = stock.history('2d')
+
+            ## calculate change in stock price (from a trading day ago)
+            deltas.append((hist['Close'][1]-hist['Close'][0])/hist['Close'][0])
+
+            ## calculate market cap
+            market_caps.append(info['sharesOutstanding'] * info['previousClose'])
+
+            ## add print statement to ensure code is running
+            print(f'downloaded {ticker}')
+        except Exception as e:
+            print(e)
+
+    return render(request, 'analysis/heatmap.html')
